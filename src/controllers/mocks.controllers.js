200f5045d01c36b6bcf650bd8174eb9ba1aaa56f
juLemucchi <julietanatalia2004@gmail.com>
2024-10-19 15:11:34 -0300

    Pre entrega Back 3

--- src/controllers/mocks.controllers.js
+++ src/controllers/mocks.controllers.js
@@ -1,6 +1,33 @@
 import MockingService from "../services/MockingService.js";
 
-const getMockingPets = (req,res)=>{
-    const pets = MockingService.getMockingPets(100);
-    
+const getMockingPets = async (req,res)=>{
+    const pets = await MockingService.getMockingPets(100);
+    res.send({status:"success",payload:pets})
 }
+
+const getMockingUsers = async (req,res) => {
+    const users =await MockingService.getMockingUsers(50);
+    res.send({status:"success",payload:users})
+}
+
+const generateData = async (req,res) => {
+    const {users, pets} = req.body;
+    try {
+
+        const mockingUsers = await MockingService.generateUsers(users);
+        const mockingPets = await MockingService.generatePets(pets);
+        
+        await Promise.all(mockingUsers.map(user => usersService.create(user)));
+        await Promise.all(mockingPets.map(pet => petsService.create(pet)));
+
+        res.send({
+            status:"success",
+            message:"Todo salio veryGood"
+        })
+        
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({status:"error", error:error.message})  
+    }    
+}
+ export default{getMockingPets, getMockingUsers, generateData}
--- src/services/mocking.js
+++ src/services/mocking.js
@@ -1,16 +1,39 @@
 import { faker } from '@faker-js/faker';
+import {createHash} from '../utils/index.js'
 
 class MockingService {
-    
-
-    async generateMockingPets(){
-        const pets=[];
-        
-        for(let i= 0; i < 10; i++){
-            pets.push({
-                name:faker.animal.dog(),
+     
+    static async generateMockingUsers(num){
+        const users=[];
+        for(let i= 0; i < num; i++){
+            users.push({
+                fistName: faker.person.firstName(),
+                lastName: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: await createHash("adopClave"),
+                role: faker.helpers.arrayElement(["user","admin"]),
+                pets: []
             })
+        }
+        return users;
+
     }
 
+    static async generateMockingPets(num){
+        const pets=[];
+        
+        for(let i= 0; i < num; i++){
+            pets.push({
+                name:faker.animal.dog(),
+                specie: faker.animal.type(),
+                adopted:false,
+                birthday: faker.date.past(),
+                image:faker.image.dog()
+            })
+    }
+    return pets;
+
 }
 }
+
+export default MockingService
